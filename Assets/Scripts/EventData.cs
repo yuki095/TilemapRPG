1324afb33e59ec141bd1aebbe2f579c44ccebc56
yuki <km95yk@gmail.com>
2021-03-27 21:59:47 +0900

    手順１２

---
+++ Assets/Scripts/EventData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class EventData
+{
+/// <summary>
+/// イベントの種類
+/// </summary>
+public enum EventType
+    {
+        Talk,       // 0
+        Search,     // 1
+    }
+
+    public EventType eventType;     // イベントの種類
+    public int no;                  // 通し番号
+    public string title;            // タイトル、NPCの名前、探す対象物の名前など
+
+    [Multiline]                     // 複数行のテキスト
+    public string dialog;           // NPCのメッセージ、対象物のメッセージなど
+
+    public Sprite eventSprite;      // イベントの画像データ
+
+    // TODO　その他
+}
---
+++ Assets/Scripts/EventDataSO.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EventDataSO", menuName = "Create EventDataSO")]
+public class EventDataSO : ScriptableObject
+{
+    public List<EventData> eventDataList = new List<EventData>();
+}
